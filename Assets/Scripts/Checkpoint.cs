ab9a3e139e9b4198ab79a2bd2dc6bc30cd54def3
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2022-02-23 22:46:44 +0100

    S7V28 Created checkpoint script

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Awake
+    private void Awake() { }
+    #endregion
+    
+    #region Start and Update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Methods
+    #endregion
+}
