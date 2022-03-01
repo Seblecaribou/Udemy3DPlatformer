a86c6a5fb5c28920afaf3c938899b6e7f1bbd6d4
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2022-03-01 23:12:32 +0100

    S12V55 Added patrolling method to enemy script and fixed skybox/lighting issue

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    #region Variables
+    public NavMeshAgent agent;
+
+    public Transform[] patrolPoints;
+    public int currentPatrolPointIndex;
+    #endregion
+
+    #region Awake
+    private void Awake() { }
+    #endregion
+
+    #region Start and Update
+    void Start()
+    {
+        SetPatrolDestination();
+    }
+
+    void Update()
+    {
+        ChangeCurrentPatrolPoint();
+    }
+    #endregion
+
+    #region Methods
+
+    private void SetPatrolDestination()
+    {     
+        agent.SetDestination(patrolPoints[currentPatrolPointIndex].position);
+    }
+
+    private void ChangeCurrentPatrolPoint()
+    {
+        if (agent.remainingDistance <= .2f)
+        {
+            currentPatrolPointIndex++;
+            if (currentPatrolPointIndex == patrolPoints.Length) currentPatrolPointIndex = 0;
+            SetPatrolDestination();
+        }
+    }
+    #endregion
+}
