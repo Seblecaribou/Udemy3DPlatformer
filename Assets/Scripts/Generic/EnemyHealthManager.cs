a817add40fbcdf834c3f258c400075e9763ccda6
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2022-03-04 00:23:04 +0100

    S12V62 Added damage to enemies. Needs refactoring in both new scripts

---
+++ Assets/Scripts/Generic/EnemyHealthManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+    #region Variables
+    public int maxHealth=1;
+    private int currentHealth;
+
+    public int deathSound;
+    #endregion
+
+    #region Awake
+    private void Awake() { }
+    #endregion
+
+    #region Start and Update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Methods
+    public void TakeDamage()
+    {
+        currentHealth--;
+        if (currentHealth <= 0)
+        {
+            AudioManager.instance.PlaySFX(true, deathSound);
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayerDamageDealer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageDealer : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Awake
+    #endregion
+
+    #region Start and Update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyHealthManager>().TakeDamage();
+        }
+    }
+    #endregion
+}
